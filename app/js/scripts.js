3db13fd37883b4bd8da1749521561a3fe601c73b
yl1tko1 <vlad-kalnaus@yandex.ua>
2017-07-15 18:56:51 +0300

    edition

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -12,6 +12,63 @@
      infinite: true,
      slidesToShow: 2,
      slidesToScroll: 2,
-     dots: true
+     dots: true,
+     responsive: [
+       {
+         breakpoint: 1024,
+         settings: {
+           slidesToShow: 2,
+           slidesToScroll: 2,
+           infinite: true,
+           dots: true
+         }
+       },
+   {
+     breakpoint: 768,
+     settings: {
+       slidesToShow: 1,
+       slidesToScroll: 1,
+       infinite: true,
+       dots: true
+     }
+   }
+ ]
+
    });
+
+ $( "#zip" ).validate({
+   rules: {
+     code: {
+       required: true
+     }
+   },
+   messages: {
+     code: {
+       required: "",
+     }
+   },
+   errorClass: 'is-valid'
+ });
+
+$('#map-form').validate({
+  rules: {
+    name: {
+      required: true,
+      minlength: 2
+    },
+    email: {
+      required: true
+    }
+  },
+  messages: {
+    name: {
+      required: "",
+    },
+    email: {
+      required: "",
+    }
+  },
+  errorClass: 'is-valid'
+});
+
  });
